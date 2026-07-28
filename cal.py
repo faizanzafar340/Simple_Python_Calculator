f864c4ab2427f2d0d17c683b6e70f2249825d1bb
Faizan Zafar <faizanzafar340@gmail.com>
2026-07-29 00:48:30 +0200

    subtraction finiished

--- cal.py
+++ cal.py
@@ -1,6 +1,7 @@
 
 
 import add
+from subB import sub
  
 def main():
     
@@ -9,7 +10,9 @@ def main():
     operation = int(input("1: Add 2: Subtract 3: Multiplicaton 4: Diivision : "))
     if operation == 1:
         print(add(a,b))
+    elif operation == 2:
+        print(sub(a,b))
     else:
-        print("results not found")
+        print("results not found") 
     
 main() 
